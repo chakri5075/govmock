3fe2e928e7da1d710205600b90a958ea50a747cd
Ankur-nbj <ankurgangwar.nbj@gmail.com>
2025-07-24 15:53:55 +0530

    modifying code to work as per Docker image

--- src/main/java/org/example/CustomerDataController.java
+++ src/main/java/org/example/CustomerDataController.java
@@ -3,13 +3,17 @@ package org.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/customer-data")
 public class CustomerDataController {
 
-   // @Autowired
-   // private CustomerDataService customerDataService;
+    private static final Logger logger = LoggerFactory.getLogger(CustomerDataController.class);
+
+    // @Autowired
+    // private CustomerDataService customerDataService;
 
     @Autowired
     private CustomerPensionDataService customerPensionDataService;
@@ -18,16 +22,21 @@ public class CustomerDataController {
 
     @PostMapping("/fetchpensiondetails")
     public ResponseEntity<CustomerPensionFormResponse> getPensionData(@RequestBody CustomerPensionFormPayload payload) {
+        logger.info("Entered getPensionData method");
         try {
             CustomerPensionFormResponse response = customerPensionDataService.fetchPensionFormData(payload);
+            logger.info("Exiting getPensionData method successfully");
             return ResponseEntity.ok(response);
         } catch (Exception e) {
+            logger.error("Exception in getPensionData method", e);
+            logger.info("Exiting getPensionData method with error");
             return ResponseEntity.internalServerError().build();
         }
     }
 
     @PostMapping("/fetchinsurancedetails")
     public ResponseEntity<InsuranceDetailsResponse> getInsuranceDetails(@RequestBody CustomerPensionFormPayload payload) {
+        logger.info("Entered getInsuranceDetails method");
         InsuranceDetailsResponse response = new InsuranceDetailsResponse();
         response.setFirstName(payload.getFirstName());
         response.setLastName(payload.getLastName());
@@ -43,12 +52,13 @@ public class CustomerDataController {
         response.setEmploymentStatus("Employed");
         response.setAmountofCover("500000");
         response.setBeneficiaryNameAndRelationship("Jane Doe - Spouse");
+        logger.info("Exiting getInsuranceDetails method successfully");
         return ResponseEntity.ok(response);
     }
 
 
-  //  @PostMapping
-   // public CustomerData getCustomerData(@RequestBody DrivingLicencePayload drivingLicence) {
-      //  return customerDataService.getCustomerDataByDrivingLicence(drivingLicence);
-   // }
+    //  @PostMapping
+    // public CustomerData getCustomerData(@RequestBody DrivingLicencePayload drivingLicence) {
+    //  return customerDataService.getCustomerDataByDrivingLicence(drivingLicence);
+    // }
 }
